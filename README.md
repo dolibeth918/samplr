1b45cef016e90650be3cc0b5843cbbd265403b99
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-09-25 10:18:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Samplr
+
+## Setup
+
+### MacOS/Linux
+
+- `npm install`
+- `npm start`
+
+### Windows
+
+- `npm install`
+- `npm run build-watch` to start the webpack process
---
+++ client/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div id="main" className="row container">
+        <h1>Hello from Spotify</h1>
+      </div>
+    );
+  }
+}
